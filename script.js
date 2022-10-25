4975b115bead715d6f39704382797b316c2edc2b
Altynai <altynaij1998@gmail.com>
2022-10-25 11:36:06 +0600

    init: template

---
+++ script.js
@@ -0,0 +1,80 @@
+/*******NaN, infinity*********/
+/*let x = 5;
+let y = 'test';
+let result = x * y;
+console.log(result);*/
+
+/*let num = 25 / 0;
+console.log(num);*/
+
+/********number**********/
+/*let first_name = 'ulan';
+let last_name = "jumabaev";
+let company = `${last_name} megacom ${first_name}`;
+console.log(company);
+*/
+
+/***** bolean *****/
+/*let isProgrammer = true;
+console.log(isProgrammer);*/
+
+/*let result = 45 > 40;
+console.log(result);*/
+
+/****** null *****/
+/*let user_name = null;
+console.log(user_name);*/
+
+/***** undefined*****/
+/*let salary;
+console.log(salary);*/
+
+/******object*****/
+/*let user = {
+    first_name:'Test',
+    last_name:'Testov',
+    isProgrammer:false
+}
+console.log(user);*/
+
+/******bigint*****/
+/*let z = 123456789123456789n;
+console.log(z);*/
+
+/******* typeof*********/
+/*let z = 123456789123456789;
+let result = typeof z;
+console.log(result);
+
+let message = 'Hello';
+console.log(typeof message);
+
+let isAdmin = false;
+console.log(typeof isAdmin);*/
+
+
+/*****Базовые математические оперции ****/
+/*let n = 45;
+let k = 5;
+console.log(n + k);
+let z = 10;
+console.log(z - 5);
+let a = 32;
+let b = 10;
+console.log(a * b);
+let num1 = 45;
+console.log(num1 / 10); */
+
+/*let salary = 325;
+let result = salary % 10;
+console.log(result);*/
+
+/*let num = 10;
+let result = num++;
+console.log(num);
+console.log(result);*/
+
+// let age = 18;
+// let result = ++age;
+// console.log(age);
+// console.log(result);
